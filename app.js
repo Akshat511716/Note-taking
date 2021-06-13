47851438d738fad7ab154b2d13f97d881c5116be
Akshat-Rana <akshat511716@gmail.com>
2021-06-13 15:36:00 +0530

    latest note comes on top feature added

--- app.js
+++ app.js
@@ -1,80 +1,65 @@
 showNotes();
 
 textarea = document.querySelector("#textInput");
-textarea.addEventListener('input', autoResize, false);
+textarea.addEventListener("input", autoResize, false);
 
 function autoResize() {
-    this.style.height ='auto';
-    this.style.height = this.scrollHeight + 'px';
+  this.style.height = "auto";
+  this.style.height = this.scrollHeight + "px";
 }
 
 let addNote = document.querySelector("#addNote");
-addNote.addEventListener('click',function(e){
+addNote.addEventListener("click", function (e) {
+  let addText = document.querySelector("#textInput");
+  let notes = localStorage.getItem("notes");
+  if (notes === null) notesObj = [];
+  else notesObj = JSON.parse(notes);
 
-    let addText = document.querySelector("#textInput");
-    let notes = localStorage.getItem("notes");
-    if(notes === null)
-        notesObj = [];
-    else
-        notesObj = JSON.parse(notes);
+  // addText.value.replace('/','');
+  notesObj.push(addText.value);
 
-    // addText.value.replace('/','');
-    notesObj.push(addText.value);
+  localStorage.setItem("notes", JSON.stringify(notesObj));
 
-    localStorage.setItem("notes", JSON.stringify(notesObj));
+  addText.value = "";
+  // console.log(notesObj);
 
-    addText.value = "";
-    // console.log(notesObj);
+  showNotes();
+});
 
-    showNotes();
-})
+function showNotes() {
+  let addText = document.querySelector("#textInput");
+  let notes = localStorage.getItem("notes");
+  if (notes === null) notesObj = [];
+  else notesObj = JSON.parse(notes);
 
-function showNotes(){
-    let notes = localStorage.getItem("notes");
-
-    if(notes === null)
-        notesObj = [];
-    else
-        notesObj = JSON.parse(notes);
-
-    let html = "";
-    notesObj.forEach(function(element,index){
-        html += `
+  let html = "";
+  notesObj.reverse().forEach(function (element, index) {
+    html += `
             <div class="note">
-                <div class="note-body">
-                    <h3>Note ${index+1}</h3>
-                    <p>${element}</p>
+                 <div class="note-body">
+                    <h3>Note ${index + 1}</h3>
+                     <p>${element}</p>
                     <button id="${index}"onclick="deleteNote(this.id)">Delete Note</button>
-                    <button id="${index}"onclick="editNote(this.id)">Edit Note</button>
-                </div>
-            </div>  
-            `
-    });
-
-    let noteElement = document.getElementById("notes");
-    if(notesObj.length !== 0)
-        noteElement.innerHTML = html;
-    else
-        noteElement.innerHTML = "Nothing to show!";
+                     <button id="${index}"onclick="editNote(this.id)">Edit Note</button>
+                 </div>
+             </div>
+                `;
+  });
+  let noteElement = document.getElementById("notes");
+  if (notesObj.length !== 0) noteElement.innerHTML = html;
+  else noteElement.innerHTML = "Nothing to show!";
 }
 
 
+function deleteNote(index) {
+  // console.log("I am deleting");
+  let notes = localStorage.getItem("notes");
+  if (notes == null) notesObj = [];
+  else notesObj = JSON.parse(notes);
 
-
-
-
-function deleteNote(index){
-    // console.log("I am deleting");
-    let notes = localStorage.getItem("notes");
-    if(notes == null)
-        notesObj = [];
-    else
-        notesObj = JSON.parse(notes);
-    
-    notesObj.splice(index, 1);
-    localStorage.setItem("notes",JSON.stringify(notesObj));
-    showNotes();
-
+  notesObj.splice(index, 1);
+  localStorage.setItem("notes", JSON.stringify(notesObj));
+  showNotes();
 }
 
 function editNote(index){
@@ -91,8 +76,5 @@ function editNote(index){
 
     deleteNote(index);
 
-    
 
-    // notesObj
-    
 }
